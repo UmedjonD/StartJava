5d54a0601c700813b4003277f2cbeafc4acf38d0
Umedjon <umedjon721@gmail.ru>
2022-10-03 19:13:23 +0300

    Lesson 3 - new structure

--- src/com/startjava/Lesson_2/animal/Wolf.java
+++ src/com/startjava/Lesson_2/animal/Wolf.java
@@ -1,24 +1,36 @@
 public class Wolf {
-
     private String gender;
     private String nickname;
     private int weight;
     private int age;
     private String color;
 
-    public Wolf() {
+    public String getGender() {
+        return gender;
     }
 
     public void setGender(String gender) {
         this.gender = gender;
     }
 
+    public String getNickname() {
+        return nickname;
+    }
+
     public void setNickname(String nickname) {
         this.nickname = nickname;
     }
 
+    public int getWeight() {
+        return weight;
+    }
+
     public void setWeight(int weight) {
         this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
     }
 
     public void setAge(int age) {
@@ -29,28 +41,12 @@ public class Wolf {
         }
     }
 
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
     public String getColor() {
         return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 
     public void go() {
